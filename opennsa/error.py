0cf345490b789f0162bd6b130fa925fe139c7beb
Henrik Thostrup Jensen <htj@nordu.net>
2011-06-12 21:24:31 +0200

    add error module

---
+++ opennsa/error.py
@@ -0,0 +1,32 @@
+"""
+Various generic errors (exceptions) for OpenNSA.
+
+Author: Henrik Thostrup Jensen <htj@nordu.net>
+Copyright: NORDUnet (2011)
+"""
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class ReservationError(Exception):
+    pass
+
+
+class CancelReservationError(Exception):
+    pass
+
+
+class ProvisionError(Exception):
+    pass
+
+
+class ReleaseProvisionError(Exception):
+    pass
+
+
+class QueryError(Exception):
+    pass
+
+
