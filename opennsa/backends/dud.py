396fd194eace86f5567bd26f24d4831ee41eeef5
Henrik Thostrup Jensen <htj@nordu.net>
2011-08-26 13:18:42 +0200

    make dud nrm backend simpler by removing connection/reservation seperation (only connection concept now)

--- opennsa/backends/dud.py
+++ opennsa/backends/dud.py
@@ -21,15 +21,20 @@ from opennsa import error as nsaerror
 ISO_DATETIME_FORMAT   = "%Y-%m-%dT%H:%M:%S:Z" # milliseconds are lacking
 
 
+# These state are internal to the NRM and cannot be shared with the NSI service layer
+RESERVED    = 'RESERVED'
+PROVISIONED = 'PROVISIONED'
 
-class _Reservation:
+
+
+class _Connection:
 
     def __init__(self, source_port, dest_port, start_time, end_time):
         self.source_port = source_port
         self.dest_port  = dest_port
         self.start_time = start_time
         self.end_time   = start_time
-
+        self.state      = RESERVED
 
 
 class DUDNSIBackend:
@@ -38,7 +43,6 @@ class DUDNSIBackend:
 
     def __init__(self, name=None):
         self.name = name
-        self.reservations = {}
         self.connections = {}
 
 
@@ -63,8 +67,9 @@ class DUDNSIBackend:
                 return True
             if res1_start_time <= res2_start_time and res1_start_time <= res2_end_time:
                 return True
+            return False
 
-        for res in self.reservations.values():
+        for res in self.connections.values():
             if source_port in [ res.source_port, res.dest_port ]:
                 if portOverlap(res.start_time, res.end_time, res_start, res_end):
                     raise nsaerror.ReserveError('Port %s not available in specified time span' % source_port)
@@ -77,50 +82,55 @@ class DUDNSIBackend:
 
 
     def reserve(self, source_port, dest_port, service_parameters):
-        reservation_id = uuid.uuid1().hex[0:8]
-        log.msg('RESERVE. IR ID: %s, Path: %s -> %s' % (reservation_id, source_port, dest_port), system='DUDBackend Network %s' % self.name)
+        conn_id = uuid.uuid1().hex[0:8]
+        log.msg('RESERVE. ICID: %s, Ports: %s -> %s' % (conn_id, source_port, dest_port), system='DUDBackend Network %s' % self.name)
         try:
             self.checkReservationFeasibility(source_port, dest_port, service_parameters.start_time, service_parameters.end_time)
         except nsaerror.ReserveError, e:
             return defer.fail(e)
 
-        res = _Reservation(source_port, dest_port, service_parameters.start_time, service_parameters.end_time)
-        self.reservations[reservation_id] = res
-        return defer.succeed(reservation_id)
+        res = _Connection(source_port, dest_port, service_parameters.start_time, service_parameters.end_time)
+        self.connections[conn_id] = res
+        return defer.succeed(conn_id)
 
 
-    def cancelReservation(self, reservation_id):
-        try:
-            self.reservations.pop(reservation_id)
-            log.msg('CANCEL. IR ID: %s' % (reservation_id), system='DUDBackend Network %s' % self.name)
-            return defer.succeed(None)
-        except KeyError:
-            raise nsaerror.CancelReservationError('No such reservation (%s)' % reservation_id)
-
-
-    def provision(self, reservation_id):
+    def provision(self, conn_id):
         # should do some time stuff sometime
         try:
-            self.reservations.pop(reservation_id)
+            conn = self.connections[conn_id]
         except KeyError:
-            raise nsaerror.ProvisionError('No such reservation (%s)' % reservation_id)
+            raise nsaerror.ProvisionError('No such connection (%s)' % conn_id)
 
-        connection_id = uuid.uuid1().hex[0:8]
-        self.connections[connection_id] = {}
-        log.msg('PROVISION. IC ID: %s, IR ID: %s' % (connection_id, reservation_id), system='DUDBackend Network %s' % self.name)
-        return defer.succeed(connection_id)
+        if conn.state != RESERVED:
+            raise nsaerror.ProvisionError('Cannot provision connection in state %s' % conn.state)
+
+        conn.state = PROVISIONED
+        log.msg('PROVISION. ICID: %s' % conn_id, system='DUDBackend Network %s' % self.name)
+        return defer.succeed(conn_id)
 
 
-    def releaseProvision(self, connection_id):
+    def releaseProvision(self, conn_id):
         try:
-            self.connections.pop(connection_id)
+            conn = self.connections.pop(conn_id)
         except KeyError:
-            raise nsaerror.ReleaseProvisionError('No such provisioned connection (%s)' % connection_id)
+            raise nsaerror.ReleaseProvisionError('No such connection (%s)' % conn_id)
 
-        reservation_id = uuid.uuid1().hex[0:8]
-        log.msg('RELEASE. IC ID: %s, IR ID: %s' % (connection_id, reservation_id), system='DUDBackend Network %s' % self.name)
-        self.reservations[reservation_id] = {} # service params can go in dict when needed
-        return defer.succeed(reservation_id)
+        if conn.state != RESERVED:
+            raise nsaerror.ProvisionError('Cannot provision connection in state %s' % conn.state)
+
+        conn.state = RESERVED
+        log.msg('RELEASE. ICID: %s' % conn_id, system='DUDBackend Network %s' % self.name)
+        self.connections[conn_id] = {} # service params can go in dict when needed
+        return defer.succeed(conn_id)
+
+
+    def cancelReservation(self, conn_id):
+        try:
+            self.connections.pop(conn_id)
+            log.msg('CANCEL. ICID : %s' % (conn_id), system='DUDBackend Network %s' % self.name)
+            return defer.succeed(None)
+        except KeyError:
+            raise nsaerror.CancelReservationError('No such reservation (%s)' % conn_id)
 
 
     def query(self, query_filter):
