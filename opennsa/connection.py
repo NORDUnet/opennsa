6e60b2da4ba77802f29b0efc488f92d17873ea0e
Henrik Thostrup Jensen <htj@nordu.net>
2011-10-05 10:57:55 +0200

    seperate out connection state into seperate module for easier reuse

--- opennsa/connection.py
+++ opennsa/connection.py
@@ -9,66 +9,14 @@ Copyright: NORDUnet (2011)
 from twisted.python import log
 from twisted.internet import defer
 
-from opennsa import error, nsa
+from opennsa import error, nsa, state
 
 
 
-# connection states
-INITIAL             = 'Initial'
-
-RESERVING           = 'Reserving'
-RESERVED            = 'Reserved'
-
-SCHEDULED           = 'Scheduled'
-AUTO_PROVISION      = 'Auto-Provision'
-PROVISIONING        = 'Provisioning'
-PROVISIONED         = 'Provisioned'
-
-RELEASING           = 'Releasing'
-CLEANING            = 'Cleaning'
-
-TERMINATING         = 'Terminateing' # John can't spell
-TERMINATED          = 'Terminated'
-
-# allowed state transitions
-TRANSITIONS = {
-    INITIAL         : [ RESERVING                                                               ],
-    RESERVING       : [ RESERVED,       TERMINATING,    CLEANING,     TERMINATED                ],
-    RESERVED        : [ SCHEDULED,      AUTO_PROVISION, PROVISIONING, TERMINATING, TERMINATED   ],
-    SCHEDULED       : [ PROVISIONING,   RELEASING,      TERMINATING,  TERMINATED                ],
-    AUTO_PROVISION  : [ PROVISIONING,   TERMINATING,    TERMINATED                              ],
-    PROVISIONING    : [ PROVISIONED,    TERMINATING,    TERMINATED                              ],
-    PROVISIONED     : [ RELEASING,      TERMINATING,    TERMINATING,                            ],
-    RELEASING       : [ RESERVED,       SCHEDULED,      TERMINATING,    TERMINATED              ],
-    CLEANING        : [ TERMINATED                                                              ],
-    TERMINATING     : [ TERMINATED,                                                             ],
-    TERMINATED      : [ ]
-}
-
-
-
-class ConnectionState:
-
-    def __init__(self, state=INITIAL):
-        self._state = state
-
-
-    def state(self):
-        return self._state
-
-
-    def switchState(self, new_state):
-        if new_state in TRANSITIONS[self._state]:
-            self._state = new_state
-        else:
-            raise error.ConnectionStateTransitionError('Transition from state %s to %s not allowed' % (self._state, new_state))
-
-
-
-class SubConnection(ConnectionState):
+class SubConnection:
 
     def __init__(self, parent_connection, connection_id, network, source_stp, dest_stp, proxy=None):
-        ConnectionState.__init__(self)
+        self.state = state.ConnectionState()
         self.parent_connection  = parent_connection
         self.connection_id      = connection_id
         self.network            = network
@@ -85,16 +33,16 @@ class SubConnection(ConnectionState):
 
         def reservationDone(int_res_id):
             log.msg('Sub-connection for network %s (%s -> %s) reserved' % (self.network, self.source_stp.endpoint, self.dest_stp.endpoint), system='opennsa.Connection')
-            self.switchState(RESERVED)
+            self.state.switchState(state.RESERVED)
             return self
 
         def reservationFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
         sub_service_params  = nsa.ServiceParameters(service_parameters.start_time, service_parameters.end_time, self.source_stp, self.dest_stp,
                                                     directionality=service_parameters.directionality, bandwidth_params=service_parameters.bandwidth_params)
-        self.switchState(RESERVING)
+        self.state.switchState(state.RESERVING)
         d = self._proxy.reservation(self.network, None, self.parent_connection.global_reservation_id, self.parent_connection.description, self.connection_id, sub_service_params)
         d.addCallbacks(reservationDone, reservationFailed)
         return d
@@ -105,14 +53,14 @@ class SubConnection(ConnectionState):
         assert self._proxy is not None, 'Proxy not set for SubConnection, cannot invoke method'
 
         def cancelDone(_):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return self
 
         def cancelFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(TERMINATING)
+        self.state.switchState(state.TERMINATING)
         d = self._proxy.terminateReservation(self.network, None, self.connection_id)
         d.addCallbacks(cancelDone, cancelFailed)
         return d
@@ -124,14 +72,14 @@ class SubConnection(ConnectionState):
 
         def provisionDone(conn_id):
             assert conn_id == self.connection_id
-            self.switchState(PROVISIONED)
+            self.state.switchState(state.PROVISIONED)
             return self
 
         def provisionFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(PROVISIONING)
+        self.state.switchState(state.PROVISIONING)
         d = self._proxy.provision(self.network, None, self.connection_id)
         d.addCallbacks(provisionDone, provisionFailed)
         return d
@@ -143,24 +91,25 @@ class SubConnection(ConnectionState):
 
         def releaseDone(conn_id):
             assert conn_id == self.connection_id
-            self.switchState(RESERVED)
+            self.state.switchState(state.RESERVED)
             return self
 
         def releaseFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(RELEASING)
+        self.state.switchState(state.RELEASING)
         d = self._proxy.releaseProvision(self.network, None, self.connection_id)
         d.addCallbacks(releaseDone, releaseFailed)
         return d
 
 
 
-class LocalConnection(ConnectionState):
+class LocalConnection: #(ConnectionState):
 
     def __init__(self, parent_connection, source_endpoint, dest_endpoint, internal_reservation_id=None, internal_connection_id=None, backend=None):
-        ConnectionState.__init__(self)
+#        ConnectionState.__init__(self)
+        self.state = state.ConnectionState()
         self.parent_connection          = parent_connection
         self.source_endpoint            = source_endpoint
         self.dest_endpoint              = dest_endpoint
@@ -178,14 +127,14 @@ class LocalConnection(ConnectionState):
 
         def reservationDone(int_res_id):
             self.internal_reservation_id = int_res_id
-            self.switchState(RESERVED)
+            self.state.switchState(state.RESERVED)
             return self
 
         def reservationFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(RESERVING)
+        self.state.switchState(state.RESERVING)
         d = self._backend.reserve(self.source_endpoint, self.dest_endpoint, service_parameters)
         d.addCallbacks(reservationDone, reservationFailed)
         return d
@@ -196,14 +145,14 @@ class LocalConnection(ConnectionState):
         assert self._backend is not None, 'Backend not set for LocalConnection, cannot invoke method'
 
         def cancelDone(_):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return self
 
         def cancelFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(TERMINATING)
+        self.state.switchState(state.TERMINATING)
         d = self._backend.cancelReservation(self.internal_reservation_id)
         d.addCallbacks(cancelDone, cancelFailed)
         return d
@@ -215,14 +164,14 @@ class LocalConnection(ConnectionState):
 
         def provisionDone(int_conn_id):
             self.internal_connection_id = int_conn_id
-            self.switchState(PROVISIONED)
+            self.state.switchState(state.PROVISIONED)
             return self
 
         def provisionFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(PROVISIONING)
+        self.state.switchState(state.PROVISIONING)
         d = self._backend.provision(self.internal_reservation_id)
         d.addCallbacks(provisionDone, provisionFailed)
         return d
@@ -235,24 +184,25 @@ class LocalConnection(ConnectionState):
         def releaseDone(int_res_id):
             self.internal_reservation_id = int_res_id
             self.internal_connection_id = None
-            self.switchState(RESERVED)
+            self.state.switchState(state.RESERVED)
             return self
 
         def releaseFailed(err):
-            self.switchState(TERMINATED)
+            self.state.switchState(state.TERMINATED)
             return err
 
-        self.switchState(RELEASING)
+        self.state.switchState(state.RELEASING)
         d = self._backend.releaseProvision(self.internal_connection_id)
         d.addCallbacks(releaseDone, releaseFailed)
         return d
 
 
 
-class Connection(ConnectionState):
+class Connection: #(ConnectionState):
 
     def __init__(self, requester_nsa, connection_id, source_stp, dest_stp, global_reservation_id=None, description=None, local_connection=None, sub_connections=None):
-        ConnectionState.__init__(self)
+#        ConnectionState.__init__(self)
+        self.state = state.ConnectionState()
         self.requester_nsa              = requester_nsa
         self.connection_id              = connection_id
         self.source_stp                 = source_stp
@@ -280,10 +230,10 @@ class Connection(ConnectionState):
         def reservationRequestsDone(results):
             successes = [ r[0] for r in results ]
             if all(successes):
-                self.switchState(RESERVED)
+                self.state.switchState(state.RESERVED)
                 return self
             else:
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 if any(successes):
                     failure_msg = ' # '.join( [ f.getErrorMessage() for success,f in results if success is False ] )
                     error_msg = 'Partial failure in reservation, may require manual cleanup (%s)' % failure_msg
@@ -293,7 +243,7 @@ class Connection(ConnectionState):
                 return defer.fail( error.ReserveError(error_msg) )
 
         self.service_parameters = service_parameters
-        self.switchState(RESERVING)
+        self.state.switchState(state.RESERVING)
 
         defs = []
         for sc in self.connections():
@@ -310,18 +260,18 @@ class Connection(ConnectionState):
         def connectionCancelled(results):
             successes = [ r[0] for r in results ]
             if all(successes):
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 if len(successes) > 1:
                     log.msg('Connection %s and all sub connections(%i) cancelled' % (self.connection_id, len(results)-1), system='opennsa.NSIService')
                 return self
             if any(successes):
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.CancelReservationError('Cancel partially failed (may require manual cleanup)')
             else:
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.CancelReservationError('Cancel failed for all local/sub connections')
 
-        self.switchState(TERMINATING)
+        self.state.switchState(state.TERMINATING)
 
         defs = []
         for sc in self.connections():
@@ -338,18 +288,18 @@ class Connection(ConnectionState):
         def provisionComplete(results):
             successes = [ r[0] for r in results ]
             if all(successes):
-                self.switchState(PROVISIONED)
+                self.state.switchState(state.PROVISIONED)
                 if len(results) > 1:
                     log.msg('Connection %s and all sub connections(%i) provisioned' % (self.connection_id, len(results)-1), system='opennsa.NSIService')
                 return self
             if any(successes):
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.ProvisionError('Provision partially failed (may require manual cleanup)')
             else:
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.ProvisionError('Provision failed for all local/sub connections')
 
-        self.switchState(PROVISIONING)
+        self.state.switchState(state.PROVISIONING)
 
         defs = []
         for sc in self.connections():
@@ -366,18 +316,18 @@ class Connection(ConnectionState):
         def connectionReleased(results):
             successes = [ r[0] for r in results ]
             if all(successes):
-                self.switchState(RESERVED)
+                self.state.switchState(state.RESERVED)
                 if len(results) > 1:
                     log.msg('Connection %s and all sub connections(%i) released' % (self.connection_id, len(results)-1), system='opennsa.NSIService')
                 return self
             if any(successes):
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.ReleaseError('Release partially failed (may require manual cleanup)')
             else:
-                self.switchState(TERMINATED)
+                self.state.switchState(state.TERMINATED)
                 raise error.ReleaseError('Release failed for all local/sub connection')
 
-        self.switchState(RELEASING)
+        self.state.switchState(state.RELEASING)
 
         defs = []
         for sc in self.connections():
---
+++ opennsa/state.py
@@ -0,0 +1,65 @@
+"""
+NSI state machine.
+
+Author: Henrik Thostrup Jensen <htj@nordu.net>
+Copyright: NORDUnet (2011)
+"""
+
+from opennsa import error
+
+
+
+# connection states
+INITIAL             = 'Initial'
+
+RESERVING           = 'Reserving'
+RESERVED            = 'Reserved'
+
+SCHEDULED           = 'Scheduled'
+AUTO_PROVISION      = 'Auto-Provision'
+PROVISIONING        = 'Provisioning'
+PROVISIONED         = 'Provisioned'
+
+RELEASING           = 'Releasing'
+CLEANING            = 'Cleaning'
+
+TERMINATING         = 'Terminateing' # John can't spell
+TERMINATED          = 'Terminated'
+
+# allowed state transitions
+TRANSITIONS = {
+    INITIAL         : [ RESERVING                                                               ],
+    RESERVING       : [ RESERVED,       TERMINATING,    CLEANING,     TERMINATED                ],
+    RESERVED        : [ SCHEDULED,      AUTO_PROVISION, PROVISIONING, TERMINATING, TERMINATED   ],
+    SCHEDULED       : [ PROVISIONING,   RELEASING,      TERMINATING,  TERMINATED                ],
+    AUTO_PROVISION  : [ PROVISIONING,   TERMINATING,    TERMINATED                              ],
+    PROVISIONING    : [ PROVISIONED,    TERMINATING,    TERMINATED                              ],
+    PROVISIONED     : [ RELEASING,      TERMINATING,    TERMINATING,                            ],
+    RELEASING       : [ RESERVED,       SCHEDULED,      TERMINATING,    TERMINATED              ],
+    CLEANING        : [ TERMINATED                                                              ],
+    TERMINATING     : [ TERMINATED,                                                             ],
+    TERMINATED      : [ ]
+}
+
+
+
+class ConnectionState:
+
+    def __init__(self, state=INITIAL):
+        self._state = state
+
+
+    def __call__(self):
+        return self.state()
+
+
+    def state(self):
+        return self._state
+
+
+    def switchState(self, new_state):
+        if new_state in TRANSITIONS[self._state]:
+            self._state = new_state
+        else:
+            raise error.ConnectionStateTransitionError('Transition from state %s to %s not allowed' % (self._state, new_state))
+
