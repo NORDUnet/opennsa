f1adb6d77128719b33630d7c257029c38d6b77fb
Henrik Thostrup Jensen <htj@nordu.net>
2011-11-02 13:27:06 +0100

    more work on junos backend, mostly ssh connection strucuture

--- opennsa/backends/junos.py
+++ opennsa/backends/junos.py
@@ -2,6 +2,33 @@
 OpenNSA JunOS backend.
 """
 
+# configure snippet:
+
+# opennsa@cph> configure <return>
+# Entering configuration mode
+#
+#[edit]
+#
+#opennsa@cph# set protocols connections interface-switch ps-to-netherlight-1780 interface ge-1/0/5.1780
+#
+#[edit]
+#
+#opennsa@cph# set interfaces ge-1/0/5 unit 1780 vlan-id 1780 encapsulation vlan-ccc
+#
+#[edit]
+#opennsa@cph# delete interfaces ge-1/0/5 unit 1780
+#
+#[edit]
+#opennsa@cph# delete protocols connections interface-switch ps-to-netherlight-1780
+#
+#[edit]
+#
+# Basically stuff should end with [edit] :-)
+#
+
+from twisted.internet import defer, protocol, reactor, endpoints
+from twisted.conch import error
+from twisted.conch.ssh import common, transport, keys, userauth, connection, channel
 
 # Stuff to get from config:
 
@@ -9,35 +36,58 @@ JUNOS_HOST              = 'cph.dcn.nordu.net'
 JUNOS_HOST_PORT         = 22
 JUNOS_HOST_FINGERPRINT  = 'd8:43:73:ee:a4:87:87:36:3f:c4:e5:3e:7c:d9:8b:d7'
 
-USERNAME    = 'opennsa'
-PUBLIC_KEY_PATH         = '/home/opennsa/.ssh/id_rsa'
-PRIVATE_KEY_PATH        = '/home/opennsa/.ssh/id_rsa.pub'
+USERNAME                = 'opennsa'
+PUBLIC_KEY_PATH         = '/home/opennsa/.ssh/id_rsa.pub'
+PRIVATE_KEY_PATH        = '/home/opennsa/.ssh/id_rsa'
 
-#PUBLIC_KEY = open(PUBLIC_KEY_PATH).read()
-#PRIVATE_KEY = open(PRIVATE_KEY_PATH).read()
-
-
-
-from twisted.internet import defer, protocol, reactor
-
-from twisted.conch import error
-from twisted.conch.ssh import common, transport, keys, userauth, connection, channel
+# commands - parametize these later
+COMMAND_CONFIGURE           = 'configure'
+COMMAND_SET_CONNECTIONS     = 'set protocols connections interface-switch ps-to-netherlight-1780 interface ge-1/0/5.1780' # connection name / interface
+COMMAND_SET_INTERFACES      = 'set interfaces ge-1/0/5 unit 1780 vlan-id 1780 encapsulation vlan-ccc'
+COMMAND_DELETE_INTERFACE    = 'delete interfaces ge-1/1/9 unit 1780'
+COMMAND_DELETE_CONNECTIONS  = 'delete protocols connections interface-switch ps-to-netherlight-1780'
 
 
 
-class ClientTransport(transport.SSHClientTransport):
 
-    def __init__(self): #, host_fingerprint):
-        self.host_fingerprint = JUNOS_HOST_FINGERPRINT
+
+
+class SSHClientTransport(transport.SSHClientTransport):
+
+    def __init__(self, fingerprints=None):
+        self.fingerprints = fingerprints or []
+        self.client_connection = None
+
 
     def verifyHostKey(self, public_key, fingerprint):
-        if fingerprint != self.host_fingerprint:
-            return defer.fail(error.ConchError('bad key'))
-        else:
+        if fingerprint in self.fingerprints:
             return defer.succeed(1)
+        else:
+            return defer.fail(error.ConchError('Fingerprint not accepted'))
+
 
     def connectionSecure(self):
-        self.requestService(ClientUserAuth('user', ClientConnection(), PUBLIC_KEY_PATH, PRIVATE_KEY_PATH))
+        self.client_connection = ClientConnection()
+        self.requestService(ClientUserAuth(USERNAME, self.client_connection, PUBLIC_KEY_PATH, PRIVATE_KEY_PATH))
+
+
+
+
+class SSHClientFactory(protocol.ClientFactory):
+
+    protocol = SSHClientTransport
+
+    def __init__(self, fingerprints=None):
+        # ClientFactory has no __init__ method, so we don't call it
+        self.fingerprints = fingerprints or []
+
+
+    def buildProtocol(self, addr):
+
+        p = self.protocol(self.fingerprints)
+        p.factory = self
+        return p
+
 
 
 
@@ -52,16 +102,17 @@ class ClientUserAuth(userauth.SSHUserAuthClient):
         return # this says we won't do password authentication
 
     def getPublicKey(self):
-        return keys.Key.fromFile(self.public_key_path).blob()
+        return keys.Key.fromFile(self.public_key_path)
 
     def getPrivateKey(self):
-        return defer.succeed(keys.Key.fromFile(self.private_key_path).keyObject)
+        return defer.succeed( keys.Key.fromFile(self.private_key_path) )
 
 
 
 class ClientConnection(connection.SSHConnection):
 
     def serviceStarted(self):
+        print "SERVICE START"
         self.openChannel(ConfigureChannel(conn = self))
 
 
@@ -73,6 +124,7 @@ class ConfigureChannel(channel.SSHChannel):
     def channelOpen(self, data):
         #d = self.conn.sendRequest(self, 'exec', common.NS('configure'), wantReply = 1)
         pass # don't do anything now, wait until we get a request
+        return self.ping()
 
 
     def ping(self):
@@ -84,25 +136,21 @@ class ConfigureChannel(channel.SSHChannel):
 #        self.catData = ''
 
     def sendEOF(self, passthru):
+        print "# SENDING EOF"
         self.conn.sendEOF(self)
         return passthru
 
 
     def closeIt(self, passthru):
-        self.conn.loseConnection()
+        #self.conn.transport.loseConnection()
         return passthru
 
-#    def _cbSendRequest(self, ignored):
-#        self.write('This data will be echoed back to us by "cat."\r\n')
-#        self.conn.sendEOF(self)
-#        self.loseConnection()
-
     def dataReceived(self, data):
-        print "DATA"
+        print "#", data,
         #self.catData += data
 
     def closed(self):
-        print "SSH Connection closed"
+        print "# SSH Connection closed"
 #        print 'We got this from "cat":', self.catData
 
 
@@ -118,17 +166,38 @@ class JunOSBackend:
         self.network_name = network_name
 
 
-    def createRouterConnection(self):
+    def createSSHConnection(self):
 
-        factory = protocol.ClientFactory()
-        factory.protocol = ClientTransport
-        reactor.connectTCP(JUNOS_HOST, JUNOS_HOST_PORT, factory)
-        return factory
+        #factory = protocol.ClientFactory()
+        #factory.protocol = ClientTransport
+        #connector = reactor.connectTCP(JUNOS_HOST, JUNOS_HOST_PORT, factory)
+        #print dir(connector)
+        #return factory
+
+        factory = SSHClientFactory( [ JUNOS_HOST_FINGERPRINT ] )
+        point = endpoints.TCP4ClientEndpoint(reactor, JUNOS_HOST, JUNOS_HOST_PORT)
+        d = point.connect(factory)
+        return d
+
+
+    def getSSHChannel(self):
+
+        def gotProtocol(p):
+            print "PROTO", p
+            print "CC", p.client_connection
+
+        d = self.createSSHConnection()
+        d.addCallback(gotProtocol)
+        return d
+#        channel = None
+#        return channel
+#        d.addCallback(gotProtocol)
 
 
     def createConnection(self, source_port, dest_port, service_parameters):
 
-        self.createRouterConnection()
+        d = self.getSSHChannel()
+        return d
 
 
 
@@ -147,18 +216,19 @@ class JunOSConnection:
 
 
     def reserve(self):
-        raise NotImplemented('constructing..')
+        err = NotImplementedError('constructing..')
+        return defer.fail(err)
 
 
     def provision(self):
-        raise NotImplemented('constructing..')
+        err = NotImplementedError('constructing..')
 
 
     def release(self):
-        raise NotImplemented('constructing..')
+        err = NotImplementedError('constructing..')
 
 
     def terminate(self):
-        raise NotImplemented('constructing..')
+        err = NotImplementedError('constructing..')
 
 
