db1c7ee4a7978a6b4d0523c7e32409fdd6e1f4e2
Henrik Thostrup Jensen <htj@nordu.net>
2011-12-15 15:19:19 +0100

    remote some stuff from setup.py and simplify it a bit by removing some old cruft

--- opennsa/setup.py
+++ opennsa/setup.py
@@ -3,67 +3,48 @@ High-level functionality for creating clients and services in OpenNSA.
 """
 
 from opennsa import nsiservice
-from opennsa.protocols.webservice import client as wsclient, service as wsservice, provider as wsprovider, requester as wsrequester, resource as wsresource
-
-
-WEBSERVICE  = 'webservice'
-JSONRPC     = 'jsonrpc'
-PROTOCOL    = WEBSERVICE
+from opennsa.protocols.webservice import client, service, provider, requester, resource
 
 
 
 def createService(network_name, topology_file, backend, host, port, wsdl_dir, ctx_factory=None):
 
-    protocol = WEBSERVICE
+    # reminds an awful lot about client setup
 
-    if protocol == WEBSERVICE:
-
-        # reminds an awful lot about client setup
-
-        if ctx_factory:
-            proto_scheme = 'https://'
-        else:
-            proto_scheme = 'http://'
-        service_url = proto_scheme + '%s:%i/NSI/services/ConnectionService' % (host,port)
-
-        resource, site = wsresource.createService()
-
-        provider_client     = wsclient.ProviderClient(service_url, wsdl_dir, ctx_factory=ctx_factory)
-        requester = wsrequester.Requester(provider_client)
-        requester_service   = wsservice.RequesterService(resource, requester)
-
-        # now provider service
-
-        nsi_service  = nsiservice.NSIService(network_name, backend, topology_file, requester)
-
-        requester_client = wsclient.RequesterClient(wsdl_dir)
-        provider = wsprovider.Provider(nsi_service, requester_client)
-        provider_service = wsservice.ProviderService(resource, provider)
-
-        return site
-
+    if ctx_factory:
+        proto_scheme = 'https://'
     else:
-        raise NotImplementedError('Unsupport protocol: %s' % protocol)
+        proto_scheme = 'http://'
+    service_url = proto_scheme + '%s:%i/NSI/services/ConnectionService' % (host,port)
+
+    nsi_resource, site = resource.createService()
+
+    provider_client     = client.ProviderClient(service_url, wsdl_dir, ctx_factory=ctx_factory)
+    nsi_requester = requester.Requester(provider_client)
+    service.RequesterService(nsi_resource, nsi_requester)
+
+    # now provider service
+
+    nsi_service  = nsiservice.NSIService(network_name, backend, topology_file, nsi_requester)
+
+    requester_client = client.RequesterClient(wsdl_dir)
+    nsi_provider = provider.Provider(nsi_service, requester_client)
+    service.ProviderService(nsi_resource, nsi_provider)
+
+    return site
 
 
 
 def createClient(host, port, wsdl_dir, ctx_factory=None):
 
-    protocol = WEBSERVICE
+    nsi_resource, site = resource.createService()
 
-    if protocol == WEBSERVICE:
+    # we only support http for callback currently
+    service_url = 'http://%s:%i/NSI/services/ConnectionService' % (host,port)
 
-        resource, site = wsresource.createService()
+    provider_client     = client.ProviderClient(service_url, wsdl_dir, ctx_factory=ctx_factory)
+    nsi_requester = requester.Requester(provider_client, callback_timeout=35)
+    service.RequesterService(nsi_resource, nsi_requester)
 
-        # we only support http for callback currently
-        service_url = 'http://%s:%i/NSI/services/ConnectionService' % (host,port)
-
-        provider_client     = wsclient.ProviderClient(service_url, wsdl_dir, ctx_factory=ctx_factory)
-        requester = wsrequester.Requester(provider_client, callback_timeout=35)
-        requester_service   = wsservice.RequesterService(resource, requester)
-
-        return requester, None, site
-
-    else:
-        raise NotImplementedError('Unsupport protocol: %s' % protocol)
+    return nsi_requester, None, site
 
