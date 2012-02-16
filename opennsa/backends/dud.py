7fe4adba8063228324dc24cc510251325eabb649
Henrik Thostrup Jensen <htj@nordu.net>
2012-02-16 16:34:50 +0100

    add commented out lines for how to create a failing dud backend

--- opennsa/backends/dud.py
+++ opennsa/backends/dud.py
@@ -51,9 +51,11 @@ class DUDConnectionManager:
     def setupLink(self, source_port, dest_port):
         log.msg('Link %s -> %s up' % (source_port, dest_port), system=self.log_system)
         return defer.succeed(None)
+        #return defer.fail(NotImplementedError('Link setup failed'))
 
 
     def teardownLink(self, source_port, dest_port):
         log.msg('Link %s -> %s down' % (source_port, dest_port), system=self.log_system)
         return defer.succeed(None)
+        #return defer.fail(NotImplementedError('Link teardown failed'))
 
