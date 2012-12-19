ee583a4be41b8e9f65543028e5387b1e900bde98
Henrik Thostrup Jensen <htj@nordu.net>
2012-12-19 13:24:25 +0100

    remove print statement from provider service

--- opennsa/protocols/nsi2/providerservice.py
+++ opennsa/protocols/nsi2/providerservice.py
@@ -80,7 +80,6 @@ class ProviderService:
             variables = None
             se = CT.ServiceExceptionType(provider_nsa, err.value.errorId, err.getErrorMessage(), variables)
             detail = helper.export(se, 'serviceException', level=4)
-            print "FAULT DETAIL", detail
         else:
             log.msg('Got a non NSIError exception, cannot create detailed fault (%s)' % type(err.value), system=LOG_SYSTEM)
             log.err(err)
