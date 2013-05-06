a1035793178f34cc39d5b45842d42cdc03af8b51
Henrik Thostrup Jensen <htj@nordu.net>
2013-05-06 14:11:49 +0200

    rewire nsiservice to use new aggregator and inline callbacks

--- opennsa/nsiservice.py
+++ opennsa/nsiservice.py
@@ -6,6 +6,7 @@ Copyright: NORDUnet (2011)
 """
 
 import uuid
+import datetime
 
 from zope.interface import implements
 
@@ -13,7 +14,7 @@ from twisted.python import log, failure
 from twisted.internet import reactor, defer, task
 
 from opennsa.interface import NSIServiceInterface
-from opennsa import nsa, error, registry, subscription, database, state, connection
+from opennsa import nsa, error, registry, subscription, database, state, aggregator
 
 
 
@@ -31,9 +32,7 @@ class NSIService:
         self.service_registry = service_registry
 
         self.topology = topology
-
-        # get own nsa from topology
-        self.nsa = self.topology.getNetwork(self.network).nsa
+        self.aggregator = aggregator.Aggregator(network, topology, service_registry)
 
         self.service_registry.registerEventHandler(registry.RESERVE,   self.reserve,   registry.SYSTEM_SERVICE)
         self.service_registry.registerEventHandler(registry.PROVISION, self.provision, registry.SYSTEM_SERVICE)
@@ -48,62 +47,13 @@ class NSIService:
 
         def gotResult(connections):
             # we should get 0 or 1 here since connection id is unique
-            if len(connections) == 1:
-                return connections[0]
-            elif len(connections) == 0:
+            if len(connections) == 0:
                 return defer.fail( error.ConnectionNonExistentError('No connection with id %s' % connection_id) )
-            else:
-                raise AssertionError('Got multiple connections for a single connection id')
+            return connections[0]
 
-        d = database.findBy(connection_id=connection_id)
+        d = database.ServiceConnection.findBy(connection_id=connection_id)
         d.addCallback(gotResult)
         return d
-
-#        if conn is None:
-#            raise error.ConnectionNonExistentError('No connection with id %s for NSA with address %s' % (connection_id, requester_nsa))
-#        else:
-#            return conn
-
-
-    def setupSubConnection(self, link, conn, service_parameters):
-
-        sub_sps = service_parameters.subConnectionClone(link.sourceSTP(), link.destSTP())
-
-        # should check for local network
-        if link.network == self.network:
-            # resolve nrm ports from the topology
-
-            # nsi2, at some point we need to move label selection into backend for now we keep it here
-            if len(link.src_labels) == 0:
-                raise error.TopologyError('Source STP must specify a label')
-            if len(link.dst_labels) == 0:
-                raise error.TopologyError('Dest STP must specify a label')
-            if len(link.src_labels) > 1:
-                raise error.TopologyError('Source STP specifies more than one label. Only one label is currently supported')
-            if len(link.dst_labels) > 1:
-                raise error.TopologyError('Dest STP specifies more than one label. Only one label is currently supported')
-
-            src_label = link.src_labels[0]
-            dst_label = link.dst_labels[0]
-            # choose a label to use :-)
-            src_label_value = str( src_label.randomLabel() )
-            dst_label_value = str( dst_label.randomLabel() )
-
-            nrm_src_port = self.topology.getNetwork(self.network).getInterface(link.src_port) + '.' + src_label_value
-            nrm_dst_port = self.topology.getNetwork(self.network).getInterface(link.dst_port) + '.' + dst_label_value
-
-            # update the connection service params to say which label was choosen here
-            sub_sps.source_stp.labels = [ nsa.Label(src_label.type_, src_label_value) ]
-            sub_sps.dest_stp.labels   = [ nsa.Label(dst_label.type_, dst_label_value) ]
-
-            sub_conn = self.backend.createConnection(nrm_src_port, nrm_dst_port, sub_sps)
-
-        else:
-            sub_conn_id = 'urn:uuid:' + str(uuid.uuid1())
-            remote_nsa = self.topology.getNetwork(link.network).nsa
-            sub_conn = connection.SubConnection(self.service_registry, self.nsa, remote_nsa, conn, sub_conn_id, link.sourceSTP(), link.destSTP(), sub_sps)
-
-        return sub_conn
 
 
     # command functionality
@@ -115,7 +65,7 @@ class NSIService:
         log.msg('Reserve request. NSA: %s. Connection ID: %s' % (requester_nsa, connection_id), system=LOG_SYSTEM)
 
         # rethink with modify...
-        connection_exists = yield database.Connection.exists(['connection_id = ?', connection_id])
+        connection_exists = yield database.ServiceConnection.exists(['connection_id = ?', connection_id])
         if connection_exists:
             raise error.ConnectionExistsError('Connection with id %s already exists' % connection_id)
 
@@ -126,93 +76,25 @@ class NSIService:
         self.topology.getNetwork(source_stp.network)
         self.topology.getNetwork(dest_stp.network)
 
-        # if (partial) local link, ensure that ports exists
+        # if the link terminates at our network, ensure that ports exists
         if source_stp.network == self.network:
             source_port = self.topology.getNetwork(self.network).getPort(source_stp.port)
         if dest_stp.network == self.network:
             dest_port = self.topology.getNetwork(self.network).getPort(dest_stp.port)
 
-        if source_stp == dest_stp:
+        if source_stp == dest_stp and source_stp.label.singleValue():
             raise error.TopologyError('Cannot connect %s to itself.' % source_stp)
 
-        conn = database.Connection(connection_id=connection_id, revision=0, global_reservation_id=global_reservation_id, description=description,
-                                   state=state.INITIAL, nsa=requester_nsa,
-                                   source_network=source_stp.network, source_port=source_stp.port, source_labels=source_stp.labels,
-                                   dest_network=dest_stp.network, dest_port=dest_stp.port, dest_labels=dest_stp.labels,
-                                   start_time=service_parameters.start_time.isoformat(), end_time=service_parameters.end_time.isoformat(),
-                                   bandwidth=service_parameters.bandwidth)
+        conn = database.ServiceConnection(connection_id=connection_id, revision=0, global_reservation_id=global_reservation_id, description=description, nsa=requester_nsa,
+                            reserve_time=datetime.datetime.utcnow(),
+                            reservation_state=state.INITIAL, provision_state=state.SCHEDULED, activation_state=state.INACTIVE, lifecycle_state=state.INITIAL,
+                            source_network=source_stp.network, source_port=source_stp.port, source_labels=source_stp.labels,
+                            dest_network=dest_stp.network, dest_port=dest_stp.port, dest_labels=dest_stp.labels,
+                            start_time=service_parameters.start_time, end_time=service_parameters.end_time,
+                            bandwidth=service_parameters.bandwidth)
         yield conn.save()
 
         # since STPs are candidates here, we need to change these later on
-
-        # figure out nature of request
-
-        path_info = ( connection_id, source_stp.network, source_stp.port, dest_stp.network, dest_stp.port, self.network)
-
-        if source_stp.network == self.network and dest_stp.network == self.network:
-            local_path_info = ( connection_id, self.network, source_stp.port, source_stp.labels, dest_stp.port, dest_stp.labels)
-            log.msg('Connection %s: Local link creation: %s %s#%s -> %s#%s' % local_path_info, system=LOG_SYSTEM)
-            link = nsa.Link(self.network, source_stp.port, dest_stp.port, source_stp.labels, dest_stp.labels)
-            sc = self.setupSubConnection(link, conn, service_parameters)
-
-            conn.source_stp.labels = sc.service_parameters.source_stp.labels
-            conn.dest_stp.labels   = sc.service_parameters.dest_stp.labels
-            conn.sub_connections.append(sc)
-
-        # This code is for chaining requests and is currently not used, but might be needed sometime in the future
-        # Once we get proper a topology service, some chaining will be necessary.
-
-        #elif source_stp.network == self.network:
-        #    # make path and chain on - common chaining
-        #    log.msg('Reserve %s: Common chain creation: %s:%s -> %s:%s (%s)' % path_info, system=LOG_SYSTEM)
-        #    paths = self.topology.findPaths(source_stp, dest_stp)
-        #    # check for no paths
-        #    paths.sort(key=lambda e : len(e.endpoint_pairs))
-        #    selected_path = paths[0] # shortest path
-        #    log.msg('Attempting to create path %s' % selected_path, system=LOG_SYSTEM)
-        #    assert selected_path.source_stp.network == self.network
-        #   # setup connection data - does this work with more than one hop?
-        #    setupSubConnection(selected_path.source_stp, selected_path.endpoint_pairs[0].sourceSTP(), conn)
-        #    setupSubConnection(selected_path.endpoint_pairs[0].destSTP(), dest_stp, conn)
-        #elif dest_stp.network == self.network:
-        #    # make path and chain on - backwards chaining
-        #    log.msg('Backwards chain creation %s: %s:%s -> %s:%s (%s)' % path_info, system=LOG_SYSTEM)
-        #    paths = self.topology.findPaths(source_stp, dest_stp)
-        #    # check for no paths
-        #    paths.sort(key=lambda e : len(e.endpoint_pairs))
-        #    selected_path = paths[0] # shortest path
-        #    log.msg('Attempting to create path %s' % selected_path, system=LOG_SYSTEM)
-        #    assert selected_path.dest_stp.network == self.network
-        #   # setup connection data
-        #    setupSubConnection(selected_path.source_stp, selected_path.endpoint_pairs[0].sourceSTP(), conn)
-        #    setupSubConnection(selected_path.endpoint_pairs[0].destSTP(), dest_stp, conn)
-        #else:
-        #    log.msg('Tree creation %s:  %s:%s -> %s:%s (%s)' % path_info, system=LOG_SYSTEM)
-
-
-        # create the connection in tree/fanout style
-        else:
-            # log about creation and the connection type
-            log.msg('Connection %s: Aggregate path creation: %s:%s -> %s:%s (%s)' % path_info, system=LOG_SYSTEM)
-            # making the connection is the same for all though :-)
-            paths = self.topology.findPaths(source_stp, dest_stp)
-
-            # error out if we could not find a path
-            if not paths:
-                error_msg = 'Could not find a path for route %s:%s -> %s:%s' % (source_stp.network, source_stp.port, dest_stp.network, dest_stp.port)
-                log.msg(error_msg, system=LOG_SYSTEM)
-                raise error.TopologyError(error_msg)
-
-            # check for no paths
-            paths.sort(key=lambda e : len(e.links()))
-            selected_path = paths[0] # shortest path
-            log.msg('Attempting to create path %s' % selected_path, system=LOG_SYSTEM)
-
-            for link in selected_path.links():
-                # fixme, need to set end labels here
-                sc = self.setupSubConnection(link, conn, service_parameters)
-                conn.sub_connections.append(sc)
-
 
         def reserveResponse(result):
             success = False if isinstance(result, failure.Failure) else True
@@ -222,11 +104,12 @@ class NSIService:
 
 
         # now reserve connections needed to create path
-        d = task.deferLater(reactor, 0, conn.reserve)
+        d = task.deferLater(reactor, 0, self.aggregator.reserve, conn)
         d.addBoth(reserveResponse)
         yield d
 
 
+    @defer.inlineCallbacks
     def provision(self, requester_nsa, provider_nsa, session_security_attr, connection_id, sub):
 
         def provisionResponse(result):
@@ -238,12 +121,13 @@ class NSIService:
         log.msg('', system=LOG_SYSTEM)
         # security check here
 
-        conn = self.getConnection(requester_nsa, connection_id)
-        d = task.deferLater(reactor, 0, conn.provision)
+        conn = yield self.getConnection(requester_nsa, connection_id)
+        d = task.deferLater(reactor, 0, self.aggregator.provision, conn)
         d.addBoth(provisionResponse)
-        return defer.succeed(None)
+        yield d
 
 
+    @defer.inlineCallbacks
     def release(self, requester_nsa, provider_nsa, session_security_attr, connection_id, sub):
 
         def releaseResponse(result):
@@ -255,12 +139,12 @@ class NSIService:
         log.msg('', system=LOG_SYSTEM)
         # security check here
 
-        conn = self.getConnection(requester_nsa, connection_id)
+        conn = yield self.getConnection(requester_nsa, connection_id)
         d = task.deferLater(reactor, 0, conn.release)
         d.addBoth(releaseResponse)
-        return defer.succeed(None)
 
 
+    @defer.inlineCallbacks
     def terminate(self, requester_nsa, provider_nsa, session_security_attr, connection_id, sub):
 
         def terminateResponse(result):
@@ -272,10 +156,9 @@ class NSIService:
         log.msg('', system=LOG_SYSTEM)
         # security check here
 
-        conn = self.getConnection(requester_nsa, connection_id)
+        conn = yield self.getConnection(requester_nsa, connection_id)
         d = task.deferLater(reactor, 0, conn.terminate)
         d.addBoth(terminateResponse)
-        return defer.succeed(None)
 
 
     def query(self, requester_nsa, provider_nsa, session_security_attr, operation, connection_ids, global_reservation_ids, sub):
