d580dbb26537a51e5dc0253defccc1f251697a17
Henrik Thostrup Jensen <htj@nordu.net>
2013-05-28 12:57:48 +0200

    add schema drop file (mainly for developers)

---
+++ datafiles/schema-delete.sql
@@ -0,0 +1,8 @@
+-- OpenNSA SQL Schema (PostgreSQL) DROPs
+-- This is mainly for development
+
+DROP TABLE simplebackendconnections;
+DROP TABLE subconnections;
+DROP TABLE service_connections;
+DROP TYPE label;
+
