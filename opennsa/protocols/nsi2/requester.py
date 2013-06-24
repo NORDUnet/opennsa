d78948441b82ebe0662ff36ab909f3dbcc92abc6
Henrik Thostrup Jensen <htj@nordu.net>
2013-06-24 16:10:47 +0200

    fix a few issues in nsi2soap requester and remove the inline strings

--- opennsa/protocols/nsi2/requester.py
+++ opennsa/protocols/nsi2/requester.py
@@ -13,6 +13,13 @@ from opennsa.interface import INSIRequester
 DEFAULT_CALLBACK_TIMEOUT = 60 # 1 minute
 URN_UUID_PREFIX = 'urn:uuid:'
 
+RESERVE         = 'reserve'
+RESERVE_COMMIT  = 'reserve_commit'
+PROVISION       = 'provision'
+RELEASE         = 'release'
+TERMINATE       = 'terminate'
+
+
 
 def createCorrelationId():
     return URN_UUID_PREFIX + str(uuid.uuid1())
@@ -72,78 +79,78 @@ class Requester:
             d.callback(result)
 
 
-    def reserve(self, service_url, nsi_header, connection_id, global_reservation_id, description, version, service_parameters):
+    def reserve(self, service_url, header, connection_id, global_reservation_id, description, version, service_parameters):
 
-        if nsi_header.correlation_id is not None:
+        if header.correlation_id is not None:
             log.msg('Reserve ignoring specified correlation id')
-        if nsi_header.reply_to is not None:
+        if header.reply_to is not None:
             log.msg('Reserve ignoring reply to')
 
-        nsi_header.correlation_id = createCorrelationId()
+        header.correlation_id = createCorrelationId()
 
         def reserveRequestFailed(err):
             # invocation failed, so we error out immediately
             log.msg('Reserve invocation failed: %s' % err.getErrorMessage())
-            self.triggerCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve', err.value)
+            self.triggerCall(header.provider_nsa, header.correlation_id, RESERVE, err.value)
 
-        rd = self.addCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve')
-        cd = self.requester_client.reserve(service_url, nsi_header, connection_id, global_reservation_id, description, version, service_parameters)
+        rd = self.addCall(header.provider_nsa, header.correlation_id, RESERVE)
+        cd = self.requester_client.reserve(service_url, header, connection_id, global_reservation_id, description, version, service_parameters)
         cd.addErrback(reserveRequestFailed)
         return rd
 
 
-    def reserveConfirmed(self, nsi_header, connection_id, global_reservation_id, description, service_parameter):
+    def reserveConfirmed(self, header, connection_id, global_reservation_id, description, service_parameter):
 
-        self.triggerCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve', connection_id)
+        self.triggerCall(header.provider_nsa, header.correlation_id, RESERVE, connection_id)
 
 
     def reserveFailed(self, correlation_id, requester_nsa, provider_nsa, connection_id, session_security_attr, err):
 
-        self.triggerCall(provider_nsa, correlation_id, 'reserve', err)
+        self.triggerCall(provider_nsa, correlation_id, RESERVE, err)
 
 
-    def reserveCommit(self, service_url, nsi_header, connection_id):
+    def reserveCommit(self, service_url, header, connection_id):
 
-        nsi_header.correlation_id = createCorrelationId()
+        header.correlation_id = createCorrelationId()
 
         def reserveCommitFailed(err):
             # invocation failed, so we error out immediately
             log.msg('ReserveCommit invocation failed: %s' % err.getErrorMessage())
-            self.triggerCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve_commit', err.value)
+            self.triggerCall(header.provider_nsa, header.correlation_id, RESERVE_COMMIT, err.value)
 
-        rd = self.addCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve_commit')
-        cd = self.requester_client.reserveCommit(service_url, nsi_header, connection_id)
+        rd = self.addCall(header.provider_nsa, header.correlation_id, RESERVE_COMMIT)
+        cd = self.requester_client.reserveCommit(service_url, header, connection_id)
         cd.addErrback(reserveCommitFailed)
         return rd
 
 
-    def reserveCommitConfirmed(self, nsi_header, connection_id):
+    def reserveCommitConfirmed(self, header, connection_id):
 
-        self.triggerCall(nsi_header.provider_nsa, nsi_header.correlation_id, 'reserve_commit', connection_id)
+        self.triggerCall(header.provider_nsa, header.correlation_id, RESERVE_COMMIT, connection_id)
 
 
-    def provision(self, requester_nsa, provider_nsa, session_security_attr, connection_id):
+    def provision(self, service_url, header, connection_id):
 
-        correlation_id = createCorrelationId()
+        header.correlation_id = createCorrelationId()
 
         def provisionRequestFailed(err):
             # invocation failed, so we error out immediately
-            self.triggerCall(provider_nsa.urn(), correlation_id, 'provision', err.value)
+            self.triggerCall(header.provider_nsa, header.correlation_id, PROVISION, err.value)
 
-        rd = self.addCall(provider_nsa, correlation_id, 'provision')
-        cd = self.requester_client.provision(correlation_id, requester_nsa, provider_nsa, session_security_attr, connection_id)
+        rd = self.addCall(header.provider_nsa, header.correlation_id, PROVISION)
+        cd = self.requester_client.provision(service_url, header, connection_id)
         cd.addErrback(provisionRequestFailed)
         return rd
 
 
     def provisionConfirmed(self, header, connection_id):
 
-        self.triggerCall(header.provider_nsa, header.correlation_id, 'provision', connection_id)
+        self.triggerCall(header.provider_nsa, header.correlation_id, PROVISION, connection_id)
 
 
     def provisionFailed(self, correlation_id, requester_nsa, provider_nsa, session_security_attr, connection_id, err):
 
-        self.triggerCall(provider_nsa, correlation_id, 'provision', err)
+        self.triggerCall(provider_nsa, correlation_id, PROVISION, err)
 
 
     def release(self, requester_nsa, provider_nsa, session_security_attr, connection_id):
