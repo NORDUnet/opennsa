f84d59d7a59855b54f09be45be5ed6b783893f60
Henrik Thostrup Jensen <htj@nordu.net>
2013-06-24 12:36:38 +0200

    update nsi2 helper to use new bindings

--- opennsa/protocols/nsi2/helper.py
+++ opennsa/protocols/nsi2/helper.py
@@ -6,20 +6,20 @@ Copyright: NORDUnet (2012)
 """
 
 import StringIO
-from xml.etree import cElementTree as ET
+from xml.etree import ElementTree as ET
 
 from twisted.python import log
 
 from opennsa import nsa, error
 from opennsa.protocols.shared import minisoap
-from opennsa.protocols.nsi2 import headertypes as HT, connectiontypes as CT
+from opennsa.protocols.nsi2 import bindings
 
 
 LOG_SYSTEM = 'NSI2.Helper'
 
 # don't really fit anywhere, consider cramming them into the bindings
-FRAMEWORK_HEADERS_NS = "http://schemas.ogf.org/nsi/2012/03/framework/headers"
-CONNECTION_TYPES_NS  = "http://schemas.ogf.org/nsi/2012/03/connection/types"
+FRAMEWORK_HEADERS_NS = "http://schemas.ogf.org/nsi/2013/04/framework/headers"
+CONNECTION_TYPES_NS  = "http://schemas.ogf.org/nsi/2013/04/connection/types"
 
 NML_ETHERNET_NS      = "http://schemas.ogf.org/nml/2012/10/ethernet#"
 
@@ -31,6 +31,7 @@ URN_NETWORK = 'urn:ogf:network:'
 
 ET.register_namespace('ftypes', FRAMEWORK_HEADERS_NS)
 ET.register_namespace('ctypes', CONNECTION_TYPES_NS)
+ET.register_namespace('nmleth', NML_ETHERNET_NS)
 
 
 
@@ -41,11 +42,11 @@ def export(type_binding, name, level=0):
     return f.getvalue()
 
 
-def createHeader(correlation_id, requester_nsa_urn, provider_nsa_urn, reply_to=None):
+def createHeader(requester_nsa_urn, provider_nsa_urn, session_security_attrs=None, reply_to=None, correlation_id=None):
 
-    header = HT.CommonHeaderType(PROTO, correlation_id, requester_nsa_urn, provider_nsa_urn, reply_to)
-    header_payload = export(header, 'nsiHeader')
-    return header_payload
+    header = bindings.CommonHeaderType(PROTO, correlation_id, requester_nsa_urn, provider_nsa_urn, reply_to, session_security_attrs)
+    header_element = header.xml(bindings.nsiHeader)
+    return header_element
 
 
 def createServiceException(err, provider_nsa):
@@ -53,17 +54,17 @@ def createServiceException(err, provider_nsa):
     variables = None
 
     if err.check(error.NSIError):
-        se = CT.ServiceExceptionType(provider_nsa, err.value.errorId, err.getErrorMessage(), variables)
+        se = bindings.ServiceExceptionType(provider_nsa, err.value.errorId, err.getErrorMessage(), variables)
     else:
         log.msg('Got a non NSIError exception: %s : %s' % (err.value.__class__.__name__, err.getErrorMessage()), system=LOG_SYSTEM)
         log.msg('Cannot create detailed service exception, defaulting to NSI InternalServerError (00500)', system=LOG_SYSTEM)
         log.err(err)
-        se = CT.ServiceExceptionType(provider_nsa, error.InternalServerError.errorId, err.getErrorMessage(), variables)
+        se = bindings.ServiceExceptionType(provider_nsa, error.InternalServerError.errorId, err.getErrorMessage(), variables)
 
     return se
 
 
-def parseRequest(soap_data, rootClass=None):
+def parseRequest(soap_data):
 
     headers, bodies = minisoap.parseSoapPayload(soap_data)
 
@@ -73,10 +74,20 @@ def parseRequest(soap_data, rootClass=None):
 
     # more checking here...
 
-    header = HT.parseString( ET.tostring( headers[0] ) )
-    body   = CT.parseString( ET.tostring( bodies[0] ), rootClass=rootClass ) # only one body element supported for now
+    header = bindings.parseElement(headers[0])
+    body   = bindings.parseElement(bodies[0])
 
-    return header, body
+    nsi_header = nsa.NSIHeader(header.requesterNSA, header.providerNSA, None, header.correlationId, header.replyTo)
+
+    return nsi_header, body
+
+
+def createLabel(type_value_pair):
+    if type_value_pair.targetNamespace:
+        label_type = '{%s}%s' % (type_value_pair.targetNamespace, type_value_pair.type_)
+    else:
+        label_type = type_value_pair.type_
+    return nsa.Label(label_type, type_value_pair.value)
 
 
 def createSTP(stp_type):
@@ -92,7 +103,7 @@ def createSTP(stp_type):
     local_id = stp_type.localId.replace(stp_type.networkId + ':', '')
 
     if stp_type.labels is not None:
-        labels = [ nsa.Label(tvp.type_, tvp.value) for tvp in stp_type.labels.attribute ]
+        labels = [ createLabel(tvp) for tvp in stp_type.labels ]
     else:
         labels = []
 
@@ -103,17 +114,16 @@ def createSTPType(stp):
 
     def createValue(v1, v2):
         if v1 == v2:
-            return v1
+            return str(v1)
         else:
             return str(v1) + '-' + str(v2)
 
     labels = None
     if stp.labels not in (None, []):
-        attributes = [ CT.TypeValuePairType(NML_ETHERNET_NS, label.type_, [ createValue(*v) for v in label.values ] ) for label in stp.labels ]
-        labels = CT.TypeValuePairListType(attributes)
+        labels = [ bindings.TypeValuePairType(NML_ETHERNET_NS, label.type_, [ createValue(*v) for v in label.values ] ) for label in stp.labels ]
 
     network = URN_NETWORK + stp.network
     port = stp.port
 
-    return CT.StpType(network, network + ':' + port, labels)
+    return bindings.StpType(network, network + ':' + port, labels)
 
