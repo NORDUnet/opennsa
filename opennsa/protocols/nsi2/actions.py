2d1a8892a380201a6347a4b113ea9bc55b5b4805
Henrik Thostrup Jensen <htj@nordu.net>
2013-06-24 12:36:16 +0200

    update actions to latest wsdl

--- opennsa/protocols/nsi2/actions.py
+++ opennsa/protocols/nsi2/actions.py
@@ -2,40 +2,45 @@
 SOAP Actions for NSI ConnectionService version 2.
 """
 
+# Provider
 
-RESERVE                 = '"http://schemas.ogf.org/nsi/2012/03/connection/service/reserve"'
+RESERVE                         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserve"'
+RESERVE_COMMIT                  = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveCommit"'
+RESERVE_ABORT                   = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveAbort"'
 
-MODIFY_CHECK            = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCheck"'
-MODIFY                  = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modify"'
-MODIFY_CANCEL           = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCancel"'
+PROVISION                       = '"http://schemas.ogf.org/nsi/2013/04/connection/service/provision"'
+RELEASE                         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/release"'
+TERMINATE                       = '"http://schemas.ogf.org/nsi/2013/04/connection/service/terminate"'
 
-PROVISION               = '"http://schemas.ogf.org/nsi/2012/03/connection/service/provision"'
-RELEASE                 = '"http://schemas.ogf.org/nsi/2012/03/connection/service/release"'
-TERMINATE               = '"http://schemas.ogf.org/nsi/2012/03/connection/service/terminate"'
+QUERY_SUMMARY                   = '"http://schemas.ogf.org/nsi/2013/04/connection/service/querySummary"'
+QUERY_SUMMARY_SYNC              = '"http://schemas.ogf.org/nsi/2013/04/connection/service/querySummarySync"'
+QUERY_RECURSIVE                 = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryRecursive"'
+QUERY_NOTIFICATION              = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryNotification"'
+QUERY_NOTIFICATION_SYNC         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryNotificationSync"'
 
-QUERY                   = '"http://schemas.ogf.org/nsi/2012/03/connection/service/query"'
+# Reqeuster
 
-RESERVE_CONFIRMED       = '"http://schemas.ogf.org/nsi/2012/03/connection/service/reserveConfirmed"'
-RESERVE_FAILED          = '"http://schemas.ogf.org/nsi/2012/03/connection/service/reserveFailed"'
 
-MODIFY_CHECK_CONFIRMED  = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCheckConfirmed"'
-MODIFY_CHECK_FAILED     = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCheckFailed"'
-MODIFY_CONFIRMED        = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyConfirmed"'
-MODIFY_FAILED           = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyFailed"'
-MODIFY_CANCEL_CONFIRMED = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCancelConfirmed"'
-MODIFY_CANCEL_FAILED    = '"http://schemas.ogf.org/nsi/2012/03/connection/service/modifyCancelFailed"'
+RESERVE_CONFIRMED               = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveConfirmed"'
+RESERVE_FAILED                  = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveFailed"'
+RESERVE_COMMIT_CONFIRMED        = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveCommitConfirmed"'
+RESERVE_COMMIT_FAILED           = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveCommitFailed"'
+RESERVE_ABORT_CONFIRMED         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveAbortConfirmed"'
 
-PROVISION_CONFIRMED     = '"http://schemas.ogf.org/nsi/2012/03/connection/service/provisionConfirmed"'
-PROVISION_FAILED        = '"http://schemas.ogf.org/nsi/2012/03/connection/service/provisionFailed"'
+PROVISION_CONFIRMED             = '"http://schemas.ogf.org/nsi/2013/04/connection/service/provisionConfirmed"'
+RELEASE_CONFIRMED               = '"http://schemas.ogf.org/nsi/2013/04/connection/service/releaseConfirmed"'
+TERMINATE_CONFIRMED             = '"http://schemas.ogf.org/nsi/2013/04/connection/service/terminateConfirmed"'
 
-RELEASE_CONFIRMED       = '"http://schemas.ogf.org/nsi/2012/03/connection/service/releaseConfirmed"'
-RELEASE_FAILED          = '"http://schemas.ogf.org/nsi/2012/03/connection/service/releaseFailed"'
+QUERY_SUMMARY_CONFIRMED         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/querySummaryConfirmed"'
+QUERY_SUMMARY_FAILED            = '"http://schemas.ogf.org/nsi/2013/04/connection/service/querySummaryFailed"'
 
-TERMINATE_CONFIRMED     = '"http://schemas.ogf.org/nsi/2012/03/connection/service/terminateConfirmed"'
-TERMINATE_FAILED        = '"http://schemas.ogf.org/nsi/2012/03/connection/service/terminateFailed"'
+QUERY_RECURSIVE_CONFIRMED       = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryRecursiveConfirmed"'
+QUERY_RECURSIVE_FAILED          = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryRecursiveFailed"'
+QUERY_NOTIFICATION_CONFIRMED    = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryNotificationConfirmed"'
 
-QUERY_CONFIRMED         = '"http://schemas.ogf.org/nsi/2012/03/connection/service/queryConfirmed"'
-QUERY_FAILED            = '"http://schemas.ogf.org/nsi/2012/03/connection/service/queryFailed"'
-
-NOTIFICATION            = '"http://schemas.ogf.org/nsi/2012/03/connection/service/notification"'
+QUERY_NOTIFICATION_FAILED       = '"http://schemas.ogf.org/nsi/2013/04/connection/service/queryNotificationFailed"'
+ERROR_EVENT                     = '"http://schemas.ogf.org/nsi/2013/04/connection/service/errorEvent"'
+DATA_PLANE_STATE_CHANGE         = '"http://schemas.ogf.org/nsi/2013/04/connection/service/dataPlaneStateChange"'
+RESERVE_TIMEOUT                 = '"http://schemas.ogf.org/nsi/2013/04/connection/service/reserveTimeout"'
+MESSAGE_DELIVERY_TIMEOUT        = '"http://schemas.ogf.org/nsi/2013/04/connection/service/messageDeliveryTimeout"'
 
