044437f9e38efa985fb7a6a05b9b99d013b031a3
Henrik Thostrup Jensen <htj@nordu.net>
2013-07-02 13:34:08 +0200

    fix nsa endpoint in service setup (not sure that endpoint is actually used)

--- opennsa/setup.py
+++ opennsa/setup.py
@@ -79,7 +79,7 @@ class OpenNSAService(twistedservice.MultiService):
         network_name = vc[config.NETWORK_NAME]
 
         base_protocol = 'https://' if vc[config.TLS] else 'http://'
-        nsa_endpoint = base_protocol + vc[config.HOST] + ':' + str(vc[config.PORT]) + '/NSI/CS2' # hardcode for now
+        nsa_endpoint = base_protocol + vc[config.HOST] + ':' + str(vc[config.PORT]) + '/NSI/services/CS2' # hardcode for now
         ns_agent = nsa.NetworkServiceAgent(network_name, nsa_endpoint)
 
         topo_source = open( vc[config.NRM_MAP_FILE] ) if type(vc[config.NRM_MAP_FILE]) is str else vc[config.NRM_MAP_FILE] # wee bit hackish
