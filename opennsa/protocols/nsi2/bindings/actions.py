f40f344de93c03b22c19b6abeefe99d15b208c95
Henrik Thostrup Jensen <htj@nordu.net>
2013-09-23 10:59:22 +0200

    update bindings to r99 of the nsi wsdl, and adopt new bindings structure

---
+++ opennsa/protocols/nsi2/bindings/actions.py
@@ -0,0 +1,46 @@
+"""
+SOAP Actions for NSI ConnectionService version 2.
+"""
+
+# Provider
+
+RESERVE                         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserve"'
+RESERVE_COMMIT                  = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveCommit"'
+RESERVE_ABORT                   = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveAbort"'
+
+PROVISION                       = '"http://schemas.ogf.org/nsi/2013/07/connection/service/provision"'
+RELEASE                         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/release"'
+TERMINATE                       = '"http://schemas.ogf.org/nsi/2013/07/connection/service/terminate"'
+
+QUERY_SUMMARY                   = '"http://schemas.ogf.org/nsi/2013/07/connection/service/querySummary"'
+QUERY_SUMMARY_SYNC              = '"http://schemas.ogf.org/nsi/2013/07/connection/service/querySummarySync"'
+QUERY_RECURSIVE                 = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryRecursive"'
+QUERY_NOTIFICATION              = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryNotification"'
+QUERY_NOTIFICATION_SYNC         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryNotificationSync"'
+
+# Reqeuster
+
+
+RESERVE_CONFIRMED               = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveConfirmed"'
+RESERVE_FAILED                  = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveFailed"'
+RESERVE_COMMIT_CONFIRMED        = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveCommitConfirmed"'
+RESERVE_COMMIT_FAILED           = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveCommitFailed"'
+RESERVE_ABORT_CONFIRMED         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveAbortConfirmed"'
+
+PROVISION_CONFIRMED             = '"http://schemas.ogf.org/nsi/2013/07/connection/service/provisionConfirmed"'
+RELEASE_CONFIRMED               = '"http://schemas.ogf.org/nsi/2013/07/connection/service/releaseConfirmed"'
+TERMINATE_CONFIRMED             = '"http://schemas.ogf.org/nsi/2013/07/connection/service/terminateConfirmed"'
+
+QUERY_SUMMARY_CONFIRMED         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/querySummaryConfirmed"'
+QUERY_SUMMARY_FAILED            = '"http://schemas.ogf.org/nsi/2013/07/connection/service/querySummaryFailed"'
+
+QUERY_RECURSIVE_CONFIRMED       = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryRecursiveConfirmed"'
+QUERY_RECURSIVE_FAILED          = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryRecursiveFailed"'
+QUERY_NOTIFICATION_CONFIRMED    = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryNotificationConfirmed"'
+QUERY_NOTIFICATION_FAILED       = '"http://schemas.ogf.org/nsi/2013/07/connection/service/queryNotificationFailed"'
+
+ERROR_EVENT                     = '"http://schemas.ogf.org/nsi/2013/07/connection/service/errorEvent"'
+DATA_PLANE_STATE_CHANGE         = '"http://schemas.ogf.org/nsi/2013/07/connection/service/dataPlaneStateChange"'
+RESERVE_TIMEOUT                 = '"http://schemas.ogf.org/nsi/2013/07/connection/service/reserveTimeout"'
+MESSAGE_DELIVERY_TIMEOUT        = '"http://schemas.ogf.org/nsi/2013/07/connection/service/messageDeliveryTimeout"'
+
