610cba57c682a231671a501b548b04a9a9297983
Henrik Thostrup Jensen <htj@nordu.net>
2014-06-11 14:28:45 +0200

    add sql script for deleting all content from tables (devel tool)

---
+++ datafiles/schema-delete.sql
@@ -0,0 +1,7 @@
+-- OpenNSA SQL Schema (PostgreSQL) DELETEs
+-- This is mainly for development
+
+DELETE FROM generic_backend_connections;
+DELETE FROM sub_connections;
+DELETE FROM service_connections;
+
