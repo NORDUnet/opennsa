8494f5e63429781a9ded4091022a99ed24a5b1c5
Henrik Thostrup Jensen <htj@nordu.net>
2017-11-07 16:13:00 +0100

    stop with the silly dynamic version

--- opennsa/__init__.py
+++ opennsa/__init__.py
@@ -1,6 +1,3 @@
-import time
-gmt = time.gmtime()
-
 # set this to "correct" version when making a release
-__version__ = 'git-%04d%02d%02d' % (gmt.tm_year, gmt.tm_mon, gmt.tm_mday)
+__version__ = '0-git'
 
