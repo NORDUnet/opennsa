09f9f9e9101473fdf6f1edad5ac280e44ef8173e
Hans Trompert <hans.trompert@surf.nl>
2021-06-15 15:09:01 +0200

    incorporated ctxfactory into opennsaTlsContext
    - servers now use the old ctxfactory through the new opennsaTlsContext
    - clients remain to use the new opennsaTlsContext

--- opennsa/opennsaTlsContext.py
+++ opennsa/opennsaTlsContext.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
-'''
-The MIT License (MIT)
-Copyright © 2021 SURF BV
+"""
+SSL/TLS context definition.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Most of this code is borrowed from the SGAS 3.X LUTS codebase.
+NORDUnet holds the copyright for SGAS 3.X LUTS and OpenNSA.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+With contributions from Hans Trompert (SURF BV)
+"""
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Author: Hans Trompert, SURF BV
-'''
-
-from OpenSSL import crypto
-from os import listdir
+from OpenSSL import crypto, SSL
+from os import listdir, path
 from sys import stdout
 from twisted.internet import ssl
 from twisted.python import log
@@ -29,18 +25,21 @@ class opennsaTlsContext:
     """
     def __init__(self, certificate_dir, verify):
 
+        self.certificate_dir = certificate_dir
+        self.verify = verify
         self._trustRoot = self._createTrustRootFromCADirectory(certificate_dir)
         self._extraCertificateOptions = {
             'enableSessions': False,
             'enableSessionTickets': False,
-            'raiseMinimumTo': ssl.TLSVersion.TLSv1_2
+            'raiseMinimumTo': ssl.TLSVersion.TLSv1_2,
+            'fixBrokenPeers': True
         }
-        if(not verify):
-            log.msg('ignoring verify=false, WILL verify peer certificate against certdir', system = LOG_SYSTEM)
 
     def _createTrustRootFromCADirectory(self, certificate_dir):
         CACertificates = []
         for CAFilename in listdir(certificate_dir):
+            if not CAFilename.endswith('.0'):
+                continue
             CAFileContent = FilePath(certificate_dir).child(CAFilename).getContent()
             try:
                 CACertificates.append(ssl.Certificate.loadPEM(CAFileContent))
@@ -59,7 +58,48 @@ class opennsaTlsContext:
         return self._extraCertificateOptions
 
     def getClientTLSOptions(self, hostname):
+        if(not self.verify):
+            log.msg('httpClient ignores verify=false, WILL verify certificate chain for %s against certdir' % (hostname), system = LOG_SYSTEM)
         return ssl.optionsForClientTLS(hostname, trustRoot=self._trustRoot, extraCertificateOptions=self._extraCertificateOptions)
+
+    def getContext(self):
+        if self.ctx is None:
+            self.ctx = self.createOpenSSLContext()
+        return self.ctx
+
+    def createOpenSSLContext(self):
+
+        log.msg('creating OpenSSL SSL Context ...', system=LOG_SYSTEM)
+
+        def verify_callback(conn, x509, error_number, error_depth, allowed):
+            # just return what openssl thinks is right
+            if self.verify:
+                return allowed # return what openssl thinks is right
+            else:
+                return 1 # allow everything which has a cert
+
+        # The way to support tls 1.0 and forward is to use the SSLv23 method
+        # (which means everything) and then disable ssl2 and ssl3
+        # Not pretty, but it works
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        ctx.set_options(SSL.OP_NO_SSLv2)
+        ctx.set_options(SSL.OP_NO_SSLv3)
+
+        # disable tls session id, as the twisted tls protocol seems to break on them
+        ctx.set_session_cache_mode(SSL.SESS_CACHE_OFF)
+        ctx.set_options(SSL.OP_NO_TICKET)
+
+        ctx.set_verify(SSL.VERIFY_PEER, verify_callback)
+
+        calist = [ ca for ca in listdir(self.certificate_dir) if ca.endswith('.0') ]
+        if len(calist) == 0 and self.verify:
+            log.msg('No certificiates loaded for CTX verificiation. CA verification will not work.', system=LOG_SYSTEM)
+        for ca in calist:
+            # openssl wants absolute paths
+            ca = path.join(self.certificate_dir, ca)
+            ctx.load_verify_locations(ca)
+
+        return ctx
 
 
 class opennsa2WayTlsContext(opennsaTlsContext):
@@ -68,6 +108,10 @@ class opennsa2WayTlsContext(opennsaTlsContext):
     over SSL/TLS.
     """
     def __init__(self, private_key_path, public_key_path, certificate_dir, verify):
+
+        self.private_key_path = private_key_path
+        self.public_key_path = public_key_path
+        self.ctx = None
 
         opennsaTlsContext.__init__(self, certificate_dir, verify)
 
@@ -85,10 +129,25 @@ class opennsa2WayTlsContext(opennsaTlsContext):
         return self.getClientCertificate().original
 
     def getClientTLSOptions(self, hostname):
+        if(not self.verify):
+            log.msg('httpClient ignores verify=false, WILL verify certificate chain for %s against certdir' % (hostname), system = LOG_SYSTEM)
         return ssl.optionsForClientTLS(hostname, trustRoot=self._trustRoot, clientCertificate=self._clientCertificate, extraCertificateOptions=self._extraCertificateOptions)
 
-    def getOpenSSLCertificateOptions(self):
-        return ssl.CertificateOptions(privateKey=self.getPrivateKey(), certificate=self.getCertificate(), trustRoot=self.getTrustRoot(),  **self.getExtraCertificateOptions())
+    def getContext(self):
+        if self.ctx is None:
+            self.ctx =  self.createOpenSSLContext()
+        return self.ctx
+
+    def createOpenSSLContext(self):
+
+        self.ctx = opennsaTlsContext.createOpenSSLContext(self)
+
+        log.msg('adding key and certificate to OpenSSL SSL Context ...', system=LOG_SYSTEM)
+        self.ctx.use_privatekey_file(self.private_key_path)
+        self.ctx.use_certificate_chain_file(self.public_key_path)
+        self.ctx.check_privatekey() # sanity check
+
+        return self.ctx
 
 
 def main():
@@ -97,7 +156,7 @@ def main():
     log.msg('trustRoot = %s' % opennsaContext.getTrustRoot(), system = LOG_SYSTEM)
     log.msg('extraCertificateOptions = %s' % opennsaContext.getExtraCertificateOptions(), system = LOG_SYSTEM)
     log.msg('clientCertificate = %s' % opennsaContext.getClientCertificate().getSubject(), system = LOG_SYSTEM)
-    log.msg('OpenSSLCertificateOptions = %s' % opennsaContext.getOpenSSLCertificateOptions(), system = LOG_SYSTEM)
+    log.msg('OpenSSLContext = %s' % opennsaContext.getContext(), system = LOG_SYSTEM)
     log.msg('ClientTLSOptions = %s' % opennsaContext.getClientTLSOptions('some.hostname'), system = LOG_SYSTEM)
 
 
